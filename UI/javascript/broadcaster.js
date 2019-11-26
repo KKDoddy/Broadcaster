c5a26da1f37ba4ee71edbe8903f96550cd9e312d
Doddy <kwizeradoddy@gmail.com>
2019-11-26 15:52:02 +0200

    ft(ui) add edit comment and edit location pages [Finishes 169953524]

--- UI/javascript/broadcaster.js
+++ UI/javascript/broadcaster.js
@@ -1,6 +1,17 @@
 const menuIcon = document.querySelector('#bar i');
 const menuList = document.querySelector('#list');
+const inputs = document.querySelectorAll('input');
 
 menuIcon.addEventListener('click', () => {
 	menuList.classList.toggle('disappear');
 });
+
+const emptyField = () => {
+	inputs.forEach( input => {
+		if(input.type !== "submit"){
+			input.value = " "
+		}
+	});
+};
+
+emptyField();
